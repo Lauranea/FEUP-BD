22f61d10f3817580cb0fa3e1bb91d64fd636335c
PBeGood4 <pedrocbeirao@gmail.com>
2022-11-16 22:24:42 +0000

    Comecar a povoar

---
+++ sql/povoar.sql
@@ -0,0 +1,82 @@
+PRAGMA foreign_keys = ON;
+
+
+
+--Table Estadio
+INSERT INTO Estadio VALUES(1, "Lusail", "Lusail Iconic Stadium");
+INSERT INTO Estadio VALUES(2, "Al Khor", "Al Bayt Stadium");
+INSERT INTO Estadio VALUES(3, "Doha", "Stadium 974");
+INSERT INTO Estadio VALUES(4, "Doha", "Al Thumama Stadium");
+INSERT INTO Estadio VALUES(5, "Al Rayyan", "Khalifa International Stadium");
+INSERT INTO Estadio VALUES(6, "Al Rayyan", "Education City Stadium");
+INSERT INTO Estadio VALUES(7, "Al Rayyan", "Ahmad bin Ali Stadium");
+INSERT INTO Estadio VALUES(8, "Al Wakrah", "Al Janoub Stadium");
+
+--Table Jogo
+
+
+--Table Jogo de fase de eliminação
+
+
+--Table Jogo de fase de grupos
+
+
+--Table Grupos
+INSERT INTO Grupos VALUES(1, 0, 0, 0, "A");
+INSERT INTO Grupos VALUES(2, 0, 0, 0, "B");
+INSERT INTO Grupos VALUES(3, 0, 0, 0, "C");
+INSERT INTO Grupos VALUES(4, 0, 0, 0, "D");
+INSERT INTO Grupos VALUES(5, 0, 0, 0, "E");
+INSERT INTO Grupos VALUES(6, 0, 0, 0, "F");
+INSERT INTO Grupos VALUES(7, 0, 0, 0, "G");
+INSERT INTO Grupos VALUES(8, 0, 0, 0, "H");
+
+--Table Equipa
+--Grupo A
+INSERT INTO Equipa VALUES(1, 1, "Catar", "Ásia", "QAT");
+INSERT INTO Equipa VALUES(2, 1, "Equador", "América", "ECU");
+INSERT INTO Equipa VALUES(3, 1, "Senegal", "África", "SEN");
+INSERT INTO Equipa VALUES(4, 1, "Países Baixos", "Europa", "NED");
+--Grupo B
+INSERT INTO Equipa VALUES(5, 2, "Inglaterra", "Europa", "ENG");
+INSERT INTO Equipa VALUES(6, 2, "Irã", "Ásia", "IRN");
+INSERT INTO Equipa VALUES(7, 2, "Estados Unidos", "América", "USA");
+INSERT INTO Equipa VALUES(8, 2, "País de Gales", "Europa", "WAL");
+--Grupo C
+INSERT INTO Equipa VALUES(9, 3, "Argentina", "América", "ARG");
+INSERT INTO Equipa VALUES(10, 3, "Arábia Saudita", "Ásia", "KSA");
+INSERT INTO Equipa VALUES(11, 3, "México", "América", "MEX");
+INSERT INTO Equipa VALUES(12, 3, "Polónia", "Europa", "POL");
+--Grupo D
+INSERT INTO Equipa VALUES(13, 4, "França", "Europa", "FRA");
+INSERT INTO Equipa VALUES(14, 4, "Austrália", "Oceania", "AUS");
+INSERT INTO Equipa VALUES(15, 4, "Dinamarca", "Europa", "DEN");
+INSERT INTO Equipa VALUES(16, 4, "Tunísia", "Ásia", "TUN");
+--Grupo E
+INSERT INTO Equipa VALUES(17, 5, "Espanha", "Europa", "SPA");
+INSERT INTO Equipa VALUES(18, 5, "Costa Rica", "América", "CRC");
+INSERT INTO Equipa VALUES(19, 5, "Alemanha", "Europa", "GER");
+INSERT INTO Equipa VALUES(20, 5, "Japão", "Ásia", "JAP");
+--Grupo F
+INSERT INTO Equipa VALUES(21, 6, "Bélgica", "Europa", "BEL");
+INSERT INTO Equipa VALUES(22, 6, "Canadá", "América", "CAN");
+INSERT INTO Equipa VALUES(23, 6, "Marrocos", "África", "MAR");
+INSERT INTO Equipa VALUES(24, 6, "Croácia", "Europa", "CRO");
+--Grupo G
+INSERT INTO Equipa VALUES(25, 7, "Brasil", "América", "BRA");
+INSERT INTO Equipa VALUES(26, 7, "Sérvia", "Europa", "SER");
+INSERT INTO Equipa VALUES(27, 7, "Suiça", "Europa", "SUI");
+INSERT INTO Equipa VALUES(28, 7, "Camarões", "África", "CMR");
+--Grupo H
+INSERT INTO Equipa VALUES(29, 8, "Portugal", "Europa", "POR");
+INSERT INTO Equipa VALUES(30, 8, "Gana", "África", "GHA");
+INSERT INTO Equipa VALUES(31, 8, "Uruguai", "América", "URU");
+INSERT INTO Equipa VALUES(32, 8, "Coreia do Sul", "Ásia", "KOR");
+
+--Table Jogador
+
+
+--Table Evento
+
+
+--Table Golo

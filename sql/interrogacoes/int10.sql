a1bdbc25faa102c39a56f66fb300fefc6d148851
jpvds1 <jpvds1@hotmail.com>
2022-12-16 22:17:33 +0000

    interrogacao 10

--- sql/interrogacoes/int10.sql
+++ sql/interrogacoes/int10.sql
@@ -2,3 +2,14 @@
 .headers on
 .nullvalue NULL
 
+with t1 as
+(
+select e.idEquipa as Equipa
+from Equipa e, Grupos g
+where g.nome = 'A' and g.idGrupo = e.idGrupo
+)
+
+select j.nome as Jogador
+from Jogador j, t1
+where j.idEquipa = t1.Equipa and j.nome like '%h%' and j.data_nascimento < '1997-01-01'
+order by j.idJogador asc;

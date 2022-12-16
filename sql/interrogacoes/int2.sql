c43892ba05a0ed07ad55038b1b481fc555dc288b
Lauranea <93668178+Lauranea@users.noreply.github.com>
2022-12-16 21:16:02 +0000

    correçao

--- sql/interrogacoes/int2.sql
+++ sql/interrogacoes/int2.sql
@@ -9,5 +9,5 @@ except
 select j.nome 
 from Jogador j, Equipa e 
 where j.idEquipa= e.idEquipa 
-and e.continente = "Europa" 
-and j.altura > 1.75 and j.altura < 1.85;
+and e.continente = 'Europa' 
+or (j.altura > 1.75 and j.altura < 1.85 and e.continente = 'Europa');
--- sql/interrogacoes/int6.sql
+++ sql/interrogacoes/int6.sql
@@ -2,3 +2,7 @@
 .headers on
 .nullvalue NULL
 
+select max(j.hora_fim-j.hora_inicio)
+from Jogo j, Jogo_elem je
+where je.idJogo_fase_elem = j.idJogo
+group by j.idJogo;

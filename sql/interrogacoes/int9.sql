5e898bb01ca394372ac9668b86d8f8726d1fae22
jpvds1 <jpvds1@hotmail.com>
2022-12-19 22:16:30 +0000

    pergunta 9 feita

--- sql/interrogacoes/int9.sql
+++ sql/interrogacoes/int9.sql
@@ -2,3 +2,20 @@
 .headers on
 .nullvalue NULL
 
+with t1 as
+(
+select Jogador.nome as Jogador, Golo.idGolo as Golo
+from Jogador left join Golo on Golo.idJogador = Jogador.idJogador
+where Golo.idGolo is not null
+order by 2 asc
+),
+
+t2 as
+(
+select t1.Jogador as Jogador, count(t1.Golo) as Golos
+from t1
+group by 1
+)
+
+select round(avg(t2.Golos),2)
+from t2;

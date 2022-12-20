514cb45ae3ebb42e46ccf9bdfaae3677331a5bdf
PBeGood4 <pedrocbeirao@gmail.com>
2022-12-20 21:16:47 +0000

    SIGLA

--- sql/interrogacoes/int8.sql
+++ sql/interrogacoes/int8.sql
@@ -4,7 +4,7 @@
 
 with t1 as
 (
-    select e1.idEquipa as pais, count(*) as maiores, e1.idGrupo as grupo
+    select e1.sigla as pais, count(*) as maiores, e1.idGrupo as grupo
     from Equipa e2, Equipa e1
     where e1.idGrupo = e2.idGrupo and e1.pontos > e2.pontos 
     group by 1
